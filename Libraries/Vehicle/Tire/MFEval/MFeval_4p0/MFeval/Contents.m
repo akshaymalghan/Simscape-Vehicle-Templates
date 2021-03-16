273227faaaaa3274dba38f8aa61fba6da8f098f4
Akshay Malghan <akshay.malghan@cevt.se>
2021-03-16 10:39:20 +0100

    trial commit

--- Libraries/Vehicle/Tire/MFEval/MFeval_4p0/MFeval/Contents.m
+++
@@ -1,2 +0,0 @@
-% MFeval
-% Version 4.0.0 (R2017b) 17-Mar-2020
--- Scripts_Data/Solver_Settings/daesscSetMultibody.m
+++
@@ -1,8 +0,0 @@
-function daesscSetMultibody(varargin)
-%daesscSetMultibody - configures daessc solver to work well with models
-% containing Simscape Multibody components. Call with () to configure for
-% use with multibody. Call with ([]) to set back to defaults. 
-
-pdaesscSetMultibody(varargin{:});
-    
-end
